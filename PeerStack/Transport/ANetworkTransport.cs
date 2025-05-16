7caf9efcec8344f8f6486f261bacf91d589f7084
myina israel sesugh <myinais@live.com>
2025-05-16 16:05:54 +0100

    Completed the Transport network protocol for TCP (If that makes any sense).

---
+++ PeerStack/Transport/ANetworkTransport.cs
@@ -0,0 +1,182 @@
+﻿using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerStack.Transport
+{
+    /// <summary>
+    ///   Metadata on an libp2p network address protocol.
+    /// </summary>
+    /// <remarks>
+    ///   Protocols are defined at <see href="https://github.com/multiformats/multiaddr/blob/master/protocols.csv"/>.
+    /// </remarks>
+    /// <seealso cref="MultiAddress"/>
+    public abstract class ANetworkTransport
+    {
+        internal static Dictionary<string, Type> Names = new();
+        internal static Dictionary<uint, Type> Codes = new();
+
+        /// <summary>
+        ///   Registers the standard network protocols for IPFS.
+        /// </summary>
+        static ANetworkTransport()
+        {
+            Register<Ipv4NetworkProtocol>();
+            Register<Ipv6NetworkProtocol>();
+            Register<TcpNetworkTransport>();
+            Register<UdpNetworkProtocol>();
+            Register<P2pNetworkProtocol>();
+            RegisterAlias<IpfsNetworkProtocol>();
+            Register<QuicNetworkProtocol>();
+            Register<HttpNetworkProtocol>();
+            Register<HttpsNetworkProtocol>();
+            Register<DccpNetworkProtocol>();
+            Register<SctpNetworkProtocol>();
+            Register<WsNetworkProtocol>();
+            Register<Libp2pWebrtcStarNetworkProtocol>();
+            Register<UdtNetworkProtocol>();
+            Register<UtpNetworkProtocol>();
+            Register<OnionNetworkProtocol>();
+            Register<Libp2pWebrtcDirectNetworkProtocol>();
+            Register<P2pCircuitNetworkProtocol>();
+            Register<DnsNetworkProtocol>();
+            Register<Dns4NetworkProtocol>();
+            Register<Dns6NetworkProtocol>();
+            Register<DnsAddrNetworkProtocol>();
+            Register<WssNetworkProtocol>();
+            Register<IpcidrNetworkProtocol>();
+        }
+
+        /// <summary>
+        ///   Register a network protocol for use.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   A <see cref="NetworkProtocol"/> to register.
+        /// </typeparam>
+        public static void Register<T>() where T : ANetworkTransport, new()
+        {
+            var protocol = new T();
+
+            if (Names.ContainsKey(protocol.Name))
+                throw new ArgumentException(string.Format("The IPFS network protocol '{0}' is already defined.", protocol.Name));
+            if (Codes.ContainsKey(protocol.Code))
+                throw new ArgumentException(string.Format("The IPFS network protocol code ({0}) is already defined.", protocol.Code));
+
+            Names.Add(protocol.Name, typeof(T));
+            Codes.Add(protocol.Code, typeof(T));
+        }
+
+        /// <summary>
+        ///   Register an alias to another network protocol.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   A <see cref="NetworkProtocol"/> to register.
+        /// </typeparam>
+        public static void RegisterAlias<T>() where T : ANetworkTransport, new()
+        {
+            var protocol = new T();
+
+            if (Names.ContainsKey(protocol.Name))
+                throw new ArgumentException(string.Format("The IPFS network protocol '{0}' is already defined.", protocol.Name));
+            if (!Codes.ContainsKey(protocol.Code))
+                throw new ArgumentException(string.Format("The IPFS network protocol code ({0}) is not defined.", protocol.Code));
+
+            Names.Add(protocol.Name, typeof(T));
+        }
+
+        /// <summary>
+        ///   The name of the protocol.
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        ///   The IPFS numeric code assigned to the network protocol.
+        /// </summary>
+        public abstract uint Code { get; }
+
+        /// <summary>
+        ///   The string value associated with the protocol.
+        /// </summary>
+        /// <remarks>
+        ///   For tcp and udp this is the port number.  This can be <b>null</b> as is the case for http and https.
+        /// </remarks>
+        public string Value { get; set; }
+
+        /// <summary>
+        ///   Writes the binary representation to the specified <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="stream">
+        ///   The <see cref="CodedOutputStream"/> to write to.
+        /// </param>
+        /// <remarks>
+        ///   The binary representation of the <see cref="Value"/>.
+        /// </remarks>
+        public abstract void WriteValue(CodedOutputStream stream);
+
+        /// <summary>
+        ///   Writes the string representation to the specified <see cref="TextWriter"/>.
+        /// </summary>
+        /// <param name="stream">
+        ///   The <see cref="TextWriter"/> to write to.
+        /// </param>
+        /// <remarks>
+        ///   The string representation of the optional <see cref="Value"/>.
+        /// </remarks>
+        public virtual void WriteValue(TextWriter stream)
+        {
+            if (Value != null)
+            {
+                stream.Write('/');
+                stream.Write(Value);
+            }
+        }
+
+        /// <summary>
+        ///   Reads the binary representation from the specified <see cref="CodedInputStream"/>.
+        /// </summary>
+        /// <param name="stream">
+        ///   The <see cref="CodedOutputStream"/> to read from.
+        /// </param>
+        /// <remarks>
+        ///   The binary representation is an option <see cref="Value"/>.
+        /// </remarks>
+        public abstract void ReadValue(CodedInputStream stream);
+
+        /// <summary>
+        ///   Reads the string representation from the specified <see cref="TextReader"/>.
+        /// </summary>
+        /// <param name="stream">
+        ///   The <see cref="TextReader"/> to read from
+        /// </param>
+        /// <remarks>
+        ///   The string representation is "/<see cref="Name"/>" followed by
+        ///   an optional "/<see cref="Value"/>".
+        /// </remarks>
+        public virtual void ReadValue(TextReader stream)
+        {
+            Value = string.Empty;
+            int c;
+            while (-1 != (c = stream.Read()) && c != '/')
+            {
+                Value += (char)c;
+            }
+        }
+
+        /// <summary>
+        ///   The <see cref="Name"/> and optional <see cref="Value"/> of the network protocol.
+        /// </summary>
+        public override string ToString()
+        {
+            using (var s = new StringWriter())
+            {
+                s.Write('/');
+                s.Write(Name);
+                WriteValue(s);
+                return s.ToString();
+            }
+        }
+    }
+}
---
+++ PeerStack/Transport/TcpNetworkTransport.cs
@@ -0,0 +1,76 @@
+﻿using Google.Protobuf;
+using System.Globalization;
+using System.Net;
+
+namespace PeerStack.Transport
+{
+    /// <summary>
+    /// TCP transport network protocol
+    /// </summary>
+    public class TcpNetworkTransport : ANetworkTransport
+    {
+       
+        /// <summary>
+        /// Network transport protocol code
+        /// </summary>
+        public override uint Code => 6;
+
+        /// <summary>
+        /// Network transport protocol name
+        /// </summary>
+        public override string Name => "tcp";
+       
+        /// <summary>
+        /// Network Port
+        /// </summary>
+        public ushort Port { get; set; }
+
+
+        /// <summary>
+        /// Read Transport protocol port value
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <exception cref="FormatException"></exception>
+        public override void ReadValue(TextReader stream)
+        {
+            base.ReadValue(stream);
+            try
+            {
+                Port = ushort.Parse(Value);
+            }
+            catch (Exception e)
+            {
+                throw new FormatException(string.Format("'{0}' is not a valid port number.", Value), e);
+            }
+        }
+
+        /// <summary>
+        /// Read Transport protocol port value
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <exception cref="NullReferenceException"></exception>
+        public override void ReadValue(CodedInputStream stream)
+        {
+            var bytes = stream.ReadPrimitiveBytes(2);
+            if(bytes is not null)
+            {
+                Port = (ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(bytes, 0));
+                Value = Port.ToString(CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                throw new NullReferenceException("Stream content not a valid port number.");
+            }
+        }
+
+        /// <summary>
+        /// Write Transport protocol port value
+        /// </summary>
+        /// <param name="stream"></param>
+        public override void WriteValue(CodedOutputStream stream)
+        {
+            var bytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)Port));
+            stream.WritePrimitiveBytes(bytes);
+        }
+    }
+}
