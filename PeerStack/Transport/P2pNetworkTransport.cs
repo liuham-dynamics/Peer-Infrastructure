1a45c31753c7b48a903f18039977d55c54274e52
myina israel sesugh <myinais@live.com>
2025-05-16 18:34:57 +0100

    Completed P2P transport network protocol

--- PeerStack/Transport/P2pNetworkTransport.cs
+++ PeerStack/Transport/P2pNetworkTransport.cs
@@ -3,6 +3,9 @@ using PeerStack.Multiformat;
 
 namespace PeerStack.Transport
 {
+    /// <summary>
+    /// P2P transport network protocol
+    /// </summary>
     public class P2pNetworkTransport : ANetworkTransport
     {
         /// <summary>
@@ -15,16 +18,25 @@ namespace PeerStack.Transport
         /// </summary>
         public override string Name => "p2p";
      
-         
-        public MultiHash MultiHash { get; private set; }
+         /// <summary>
+         /// Multi-hash
+         /// </summary>
+        public MultiHash? MultiHash { get; private set; }
 
-
+        /// <summary>
+        /// Read p2p values from stream
+        /// </summary>
+        /// <param name="stream">TextReader</param>
         public override void ReadValue(TextReader stream)
         {
             base.ReadValue(stream);
             MultiHash = new MultiHash(Value);
         }
 
+        /// <summary>
+        /// Read p2p values from primitive stream
+        /// </summary>
+        /// <param name="stream">CodedInputStream</param>
         public override void ReadValue(CodedInputStream stream)
         {
             stream.ReadLength();
@@ -32,11 +44,18 @@ namespace PeerStack.Transport
             Value = MultiHash.ToBase58();
         }
 
+        /// <summary>
+        /// Write p2p values to stream
+        /// </summary>
+        /// <param name="stream">CodedOutputStream</param>
         public override void WriteValue(CodedOutputStream stream)
         {
-            var bytes = MultiHash.ToArray();
-            stream.WriteLength(bytes.Length);
-            stream.WriteSomeBytes(bytes);
+            var bytes = MultiHash?.ToArray();
+            if(bytes is not null)
+            {
+                stream.WriteLength(bytes.Length);
+                stream.WritePrimitiveBytes(bytes);
+            }
         }
     }
 }
