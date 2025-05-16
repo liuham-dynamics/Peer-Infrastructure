107eee151d906c3eff2b15bc100a0ec4208bdb0f
myina israel sesugh <myinais@live.com>
2025-05-16 16:11:00 +0100

    Completed the UDP network transport protocol

---
+++ PeerStack/Transport/UdpNetworkTransport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerStack.Transport
+{
+    /// <summary>
+    /// UDP transport network protocol
+    /// </summary>
+    public class UdpNetworkTransport : TcpNetworkTransport
+    {
+        /// <summary>
+        /// Network transport protocol code
+        /// </summary>
+        public override uint Code => 273;
+
+        /// <summary>
+        /// Network transport protocol name
+        /// </summary>
+        public override string Name => "udp";
+      
+    }
+}
