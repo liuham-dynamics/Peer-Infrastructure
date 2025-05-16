9e57d7f98b2488b53eea10dc282521d79053b00c
myina israel sesugh <myinais@live.com>
2025-05-16 16:25:33 +0100

    Implemented the IP transport network protocol's abstract class.

---
+++ PeerStack/Transport/AIpNetworkTransport.cs
@@ -0,0 +1,64 @@
+﻿using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerStack.Transport
+{
+    public abstract class AIpNetworkTransport : ANetworkTransport
+    {
+        /// <summary>
+        /// An internet protocol address
+        /// </summary>
+        public required IPAddress Address { get; set; }
+
+        /// <summary>
+        /// Read address from stream
+        /// </summary>
+        /// <param name="stream">TextReader</param>
+        /// <exception cref="FormatException"></exception>
+        public override void ReadValue(TextReader stream)
+        {
+            base.ReadValue(stream);
+            try
+            {
+                // Remove the scope id.
+                int i = Value.LastIndexOf('%');
+                if (i != -1)
+                {
+                    Value = Value[..i];
+                }
+
+                // parse
+                Address = IPAddress.Parse(Value);
+            }
+            catch (Exception e)
+            {
+                throw new FormatException(string.Format("'{0}' is not a valid IP address.", Value), e);
+            }
+        }
+
+        /// <summary>
+        /// Write address to stream
+        /// </summary>
+        /// <param name="stream">TextWriter</param>
+        public override void WriteValue(TextWriter stream)
+        {
+            stream.Write('/');
+            stream.Write(Address.ToString());
+        }
+
+        /// <summary>
+        /// Write address to primitive stream
+        /// </summary>
+        /// <param name="stream">CodedOutputStream</param>
+        public override void WriteValue(CodedOutputStream stream)
+        {
+            var ip = Address.GetAddressBytes();
+            stream.WritePrimitiveBytes(ip);
+        }
+    }
+}
